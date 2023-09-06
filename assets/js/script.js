e0e4b0282c7c5149d4c83fc90a6b886619a99b07
tamariam <>
2023-09-06 10:09:08 +0000

    add js code to select elements and chek with console log

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+let boxOne = document.querySelector("#box-1");
+console.log(boxOne);
+let boxTwo = document.querySelector("#box-2");
+console.log(boxTwo);
+let boxThree = document.querySelector('#box-3');
+console.log(boxThree);
