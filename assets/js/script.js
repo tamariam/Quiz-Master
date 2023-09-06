d10e71e6a38caf03356d870677e7c091374d0e99
tamariam <>
2023-09-06 12:48:36 +0000

    add  eventlisteners to menu and boxes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,43 @@
 let boxOne = document.querySelector("#box-1");
-console.log(boxOne);
 let boxTwo = document.querySelector("#box-2");
-console.log(boxTwo);
 let boxThree = document.querySelector('#box-3');
-console.log(boxThree);
+let menuBtn = document.querySelector('.menu-btn');
+let mainBoard = document.querySelector('.game-menu');
+let colorBoard = document.querySelector('.color-div');
+let colorOptions = document.querySelector('.color-btn');
+
+colorOptions.addEventListener('click', () => {
+    mainBoard.style.display = 'none';
+    colorBoard.style.display = 'flex';
+
+});
+menuBtn.addEventListener('click', () => {
+    colorBoard.style.display = 'none';
+    mainBoard.style.display = 'flex';
+});
+const changeBorder = (box, boxes) => {
+    for (let sopo of boxes) {
+        sopo.classList.remove('active');
+    }
+    box.classList.add('active');
+};
+boxTwo.addEventListener('click', () => {
+    mainBoard.style.backgroundColor = '#e76f51';
+    document.body.style.backgroundColor = '#cdb4db';
+    colorBoard.style.backgroundColor = '#e76f51';
+    changeBoarder(boxTwo);
+    // boxTwo.style.border = '1px dotted #ffffff';
+});
+boxThree.addEventListener('click', () => {
+    mainBoard.style.backgroundColor = '#bb3e03';
+    document.body.style.backgroundColor = '#ffd670';
+    colorBoard.style.backgroundColor = '#bb3e03';
+    // boxThree.style.border = '2px dotted #ffffff';
+});
+boxOne.addEventListener('click', () => {
+    mainBoard.style.backgroundColor = ' #fca311';
+    document.body.style.backgroundColor = ' #ffcdb2';
+    colorBoard.style.backgroundColor = '#fca311';
+    // boxOne.style.border = '2px dotted #ffffff';
+});
+
