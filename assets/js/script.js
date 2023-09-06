3f6d6ce60721a0793a5b9d5c39021eba8dfb42d2
tamariam <>
2023-09-06 14:56:37 +0000

    add responsive styles to color-div and its elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+//get elements//
 let boxOne = document.querySelector("#box-1");
 let boxTwo = document.querySelector("#box-2");
 let boxThree = document.querySelector('#box-3');
@@ -6,6 +7,7 @@ let mainBoard = document.querySelector('.game-menu');
 let colorBoard = document.querySelector('.color-div');
 let colorOptions = document.querySelector('.color-btn');
 let boxes = document.querySelectorAll('.box');
+//this function changes border, to  color div boxes when click//
 const borderChange = (activeBox) => {
     for (let box of boxes) {
         if (box !== activeBox) {
@@ -14,35 +16,38 @@ const borderChange = (activeBox) => {
     }
     activeBox.classList.add('active');
 };
+//add eventlistener to colorOptions , when user press colorOptions btn on mainBoard, mainBoard disapaears and colorBoard displays.//
 colorOptions.addEventListener('click', () => {
-    mainBoard.style.display = 'none'; ('box');
+    mainBoard.style.display = 'none';
     colorBoard.style.display = 'flex';
 
 });
+//  when user click menuBtn ,mainBoard will display and colorBoard disapears//
 menuBtn.addEventListener('click', () => {
     colorBoard.style.display = 'none';
     mainBoard.style.display = 'flex';
 });
+//add eventListeners to boxes to change color//
 boxOne.addEventListener('click', () => {
     mainBoard.style.backgroundColor = ' #fca311';
     document.body.style.backgroundColor = ' #ffcdb2';
     colorBoard.style.backgroundColor = '#fca311';
+    menuBtn.style.backgroundColor = '#e76f51';
     borderChange(boxOne);
-    // boxOne.style.border = '2px dotted #ffffff';
+
 });
 boxTwo.addEventListener('click', () => {
     mainBoard.style.backgroundColor = '#e76f51';
     document.body.style.backgroundColor = '#cdb4db';
     colorBoard.style.backgroundColor = '#e76f51';
     borderChange(boxTwo);
-    // boxTwo.style.border = '1px dotted #ffffff';
 });
 boxThree.addEventListener('click', () => {
     mainBoard.style.backgroundColor = '#bb3e03';
     document.body.style.backgroundColor = '#ffd670';
     colorBoard.style.backgroundColor = '#bb3e03';
     borderChange(boxThree);
-    // boxThree.style.border = '2px dotted #ffffff';
+
 });
 
 
