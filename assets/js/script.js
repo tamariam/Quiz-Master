b9d52fceac339089a3bb14779b3ee35ef22eca7e
tamariam <>
2023-09-12 09:37:29 +0000

    create array of sports questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,7 @@ let colorBoard = document.querySelector('.color-div');
 let colorOptions = document.querySelector('.color-btn');
 let boxes = document.querySelectorAll('.box');
 let sportBtn = document.querySelector('.sport-btn');
-let sportBoard = document.querySelector('.main-div');
+let gameBoard = document.querySelector('.main-div');
 let userScoresDiv = document.querySelector('.user-score');
 let questionWindow = document.querySelector('.question-window');
 let playDiv = document.querySelector('.play-div');
@@ -65,13 +65,13 @@ noBtn.addEventListener('click', () => {
 
 });
 //create function goToGameBoard amd we will use this function to go gameBoard when user clicks one of the btns from music movie or sport//
-const goToGameBoard=()=>{
+const goToGameBoard = () => {
     mainBoard.style.display = 'none';
-    sportBoard.style.display = 'flex';
-}
-movieBtn.addEventListener('click', goToGameBoard)
-sportBtn.addEventListener('click',goToGameBoard);
-musicBtn.addEventListener('click',  goToGameBoard);
+    gameBoard.style.display = 'flex';
+};
+movieBtn.addEventListener('click', goToGameBoard);
+sportBtn.addEventListener('click', goToGameBoard);
+musicBtn.addEventListener('click', goToGameBoard);
 //this function changes border, to  color div boxes when click//
 const borderChange = (activeBox) => {
     for (let box of boxes) {
@@ -89,7 +89,7 @@ colorOptions.addEventListener('click', () => {
 });
 //  when user click menuBtn ,mainBoard will display and colorBoard disapears//
 menuBtn.addEventListener('click', () => {
-    sportBoard.style.display = 'none';
+    gameBoard.style.display = 'none';
     colorBoard.style.display = 'none';
     mainBoard.style.display = 'flex';
     playDiv.style.display = 'none';
@@ -97,7 +97,7 @@ menuBtn.addEventListener('click', () => {
 });
 
 sportMenuBtn.addEventListener('click', () => {
-    sportBoard.style.display = 'none';
+    gameBoard.style.display = 'none';
     colorBoard.style.display = 'none';
     mainBoard.style.display = 'flex';
     questionWindow.style.display = 'block';
@@ -113,7 +113,7 @@ boxOne.addEventListener('click', () => {
     mainBoard.style.backgroundColor = ' #fca311';
     document.body.style.backgroundColor = ' #ffcdb2';
     colorBoard.style.backgroundColor = '#fca311';
-    sportBoard.style.backgroundColor = '#fca311';
+    gameBoard.style.backgroundColor = '#fca311';
     borderChange(boxOne);
 
 });
@@ -121,16 +121,69 @@ boxTwo.addEventListener('click', () => {
     mainBoard.style.backgroundColor = '#e76f51';
     document.body.style.backgroundColor = '#cdb4db';
     colorBoard.style.backgroundColor = '#e76f51';
-    sportBoard.style.backgroundColor = '#e76f51';
+    gameBoard.style.backgroundColor = '#e76f51';
     borderChange(boxTwo);
 });
 boxThree.addEventListener('click', () => {
     mainBoard.style.backgroundColor = '#bb3e03';
     document.body.style.backgroundColor = '#ffd670';
     colorBoard.style.backgroundColor = '#bb3e03';
-    sportBoard.style.backgroundColor = '#bb3e03';
+    gameBoard.style.backgroundColor = '#bb3e03';
     borderChange(boxThree);
 
 });
+///// MAIN GAIOM LOGIC////
+const sportQuestions = [
+    {
+        question: "Who holds the record for the most goals scored in a single FIFA World Cup tournament?",
+        choices: ["a) Lionel Messi", "b) Cristiano Ronaldo", "c) Just Fontaine"],
+        correctAnswer: "c"
+    },
+    {
+        question: "In which sport would you perform a slam dunk?",
+        choices: ["a) Tennis", "b) Basketball", "c) Golf"],
+        correctAnswer: "b"
+    },
+    {
+        question: "Which country won the most gold medals in the 2016 Summer Olympics?",
+        choices: ["a) United States", "b) China", "c) Russia"],
+        correctAnswer: "a"
+    },
+    {
+        question: "What is the diameter of a standard basketball hoop in inches?",
+        choices: ["a) 16 inches", "b) 18 inches", "c) 20 inches"],
+        correctAnswer: "b"
+    },
+    {
+        question: "Who is often referred to as 'The Greatest' in the world of boxing?",
+        choices: ["a) Mike Tyson", "b) Muhammad Ali", "c) Floyd Mayweather"],
+        correctAnswer: "b"
+    },
+    {
+        question: "In which year did the first modern Olympic Games take place?",
+        choices: ["a) 1886", "b) 1900", "c) 1896"],
+        correctAnswer: "c"
+    },
+    {
+        question: "What is the national sport of Japan?",
+        choices: ["a) Judo", "b) Sumo wrestling", "c) Karate"],
+        correctAnswer: "b"
+    },
+    {
+        question: "Which team has won the most Super Bowl championships in NFL history?",
+        choices: ["a) San Francisco 49ers", "b) New England Patriots", "c) Pittsburgh Steelers"],
+        correctAnswer: "b"
+    },
+    {
+        question: "What is the distance of a marathon race in kilometers?",
+        choices: ["a) 26.2 kilometers", "b) 42.2 kilometers", "c) 50 kilometers"],
+        correctAnswer: "a"
+    },
+    {
+        question: "Who is the all-time leading scorer in the NBA?",
+        choices: ["a) LeBron James", "b) Kobe Bryant", "c) Kareem Abdul-Jabbar"],
+        correctAnswer: "c"
+    }
+];
 
 
